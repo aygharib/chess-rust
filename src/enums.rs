5afee67540e2279b5f09189cba456a221c023813
aygharib <12767433+aygharib@users.noreply.github.com>
2024-11-28 16:29:07 -0500

    add ability to print pieces in a nice manner

--- src/enums.rs
+++ src/enums.rs
@@ -1,7 +1,13 @@
+use colored::*;
+use core::fmt;
+
 #[derive(Debug)]
 pub enum Piece {
-    Rook(Color),
+    Pawn(Color),
+    Knight(Color),
     Bishop(Color),
+    Rook(Color),
+    Queen(Color),
     King(Color),
 }
 
@@ -9,4 +15,46 @@ pub enum Piece {
 pub enum Color {
     White,
     Black,
+}
+
+impl fmt::Display for Piece {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let (letter, color) = match self {
+            Piece::Pawn(color) => ('P', color),
+            Piece::Knight(color) => ('N', color),
+            Piece::Bishop(color) => ('B', color),
+            Piece::Rook(color) => ('R', color),
+            Piece::Queen(color) => ('Q', color),
+            Piece::King(color) => ('K', color),
+        };
+
+        write!(
+            f,
+            "{}",
+            format!("{}", letter).color(match color {
+                Color::White => "white",
+                Color::Black => "black",
+            })
+        )
+
+        // println!("{}", "Too small".red());
+
+        // match color {
+        //     Color::White => {
+        //         write!(f, "{}", format!("{}", letter.to_string().white()))
+        //     }
+        //     Color::Black => {
+        //         write!(f, "{}", format!("{}", letter.to_string().black()))
+        //     }
+        // }
+
+        // write!(
+        //     f,
+        //     "{}",
+        //     format!("{}", letter).to_string().color(match color {
+        //         Color::White => "green",
+        //         Color::Black => "red",
+        //     })
+        // )
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,99 @@ mod enums;
 use enums::{Color, Piece};
 
 fn main() {
+    // let board = [
+    //     [
+    //         Piece::Rook(Color::White),
+    //         Piece::Knight(Color::White),
+    //         Piece::Bishop(Color::White),
+    //         Piece::Queen(Color::White),
+    //         Piece::King(Color::White),
+    //         Piece::Bishop(Color::White),
+    //         Piece::Knight(Color::White),
+    //         Piece::Rook(Color::White),
+    //     ],
+    //     [
+    //         Piece::Pawn(Color::White),
+    //         Piece::Pawn(Color::White),
+    //         Piece::Pawn(Color::White),
+    //         Piece::Pawn(Color::White),
+    //         Piece::Pawn(Color::White),
+    //         Piece::Pawn(Color::White),
+    //         Piece::Pawn(Color::White),
+    //         Piece::Pawn(Color::White),
+    //     ],
+    //     [
+    //         Piece::Pawn(Color::Black),
+    //         Piece::Pawn(Color::Black),
+    //         Piece::Pawn(Color::Black),
+    //         Piece::Pawn(Color::Black),
+    //         Piece::Pawn(Color::Black),
+    //         Piece::Pawn(Color::Black),
+    //         Piece::Pawn(Color::Black),
+    //         Piece::Pawn(Color::Black),
+    //     ],
+    //     [
+    //         Piece::Rook(Color::Black),
+    //         Piece::Knight(Color::Black),
+    //         Piece::Bishop(Color::Black),
+    //         Piece::Queen(Color::Black),
+    //         Piece::King(Color::Black),
+    //         Piece::Bishop(Color::Black),
+    //         Piece::Knight(Color::Black),
+    //         Piece::Rook(Color::Black),
+    //     ],
+    // ];
+
+    let board = [
+        [
+            Some(Piece::Rook(Color::White)),
+            Some(Piece::Knight(Color::White)),
+            Some(Piece::Bishop(Color::White)),
+            Some(Piece::Queen(Color::White)),
+            Some(Piece::King(Color::White)),
+            Some(Piece::Bishop(Color::White)),
+            Some(Piece::Knight(Color::White)),
+            Some(Piece::Rook(Color::White)),
+        ],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [
+            Some(Piece::Pawn(Color::White)),
+            Some(Piece::Pawn(Color::White)),
+            Some(Piece::Pawn(Color::White)),
+            Some(Piece::Pawn(Color::White)),
+            Some(Piece::Pawn(Color::White)),
+            Some(Piece::Pawn(Color::White)),
+            Some(Piece::Pawn(Color::White)),
+            Some(Piece::Pawn(Color::White)),
+        ],
+        [
+            Some(Piece::Pawn(Color::Black)),
+            Some(Piece::Pawn(Color::Black)),
+            Some(Piece::Pawn(Color::Black)),
+            Some(Piece::Pawn(Color::Black)),
+            Some(Piece::Pawn(Color::Black)),
+            Some(Piece::Pawn(Color::Black)),
+            Some(Piece::Pawn(Color::Black)),
+            Some(Piece::Pawn(Color::Black)),
+        ],
+        [
+            Some(Piece::Rook(Color::Black)),
+            Some(Piece::Knight(Color::Black)),
+            Some(Piece::Bishop(Color::Black)),
+            Some(Piece::Queen(Color::Black)),
+            Some(Piece::King(Color::Black)),
+            Some(Piece::Bishop(Color::Black)),
+            Some(Piece::Knight(Color::Black)),
+            Some(Piece::Rook(Color::Black)),
+        ],
+    ];
+
+    // println!("{:#?}", board);
+    println!("{:?}", board);
+
     let piece = Piece::Rook(Color::White);
-    println!("{:#?}", piece);
+    println!("{}", piece);
 }
